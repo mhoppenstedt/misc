082302b8ca54631873d3fd0f49265e82c29f1b4f
Mitchell Hoppenstedt <hoppenstedt.mitch@gmail.com>
2019-01-09 09:40:44 -0600

    adding .gitignore file and employees.csv; updating files

--- main.go
+++ main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"html/template"
 	"net/http"
+	"os"
 )
 
-func main() {
-	http.Handle("/", http.HandlerFunc(myHandleFunc))
+type questEmployee struct {
+	First string
+	Last  string
+	Dept  string
+}
 
+var employees []questEmployee
+
+func main() {
+	csvImport("employees.csv")
+	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func myHandleFunc(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("This is inside myHandleFunc")
+func csvImport(in string) {
+	f, err := os.Open(in)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	emps, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, v := range emps {
+		e := questEmployee{
+			First: v[0],
+			Last:  v[1],
+			Dept:  v[2],
+		}
+
+		employees = append(employees, e)
+	}
+}
+
+func indexHandler(res http.ResponseWriter, req *http.Request) {
+	t, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	t.Execute(res, employees)
+	fmt.Println("Served")
 }
